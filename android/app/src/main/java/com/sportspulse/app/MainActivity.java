638efc7b4da3cb112e42c2f06216e6f331c5a407
boossbro <meshackbossbro@gmail.com>
2026-01-06 08:03:49 +0000

    Fix build and prepare APK

--- android/app/src/main/java/com/sportspulse/app/MainActivity.java
+++ android/app/src/main/java/com/sportspulse/app/MainActivity.java
@@ -1,5 +1,26 @@
-package com.sportspulse.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
+package com.sportspulse.app; 
+import android.os.Bundle; import 
+com.getcapacitor.BridgeActivity; 
+public class MainActivity extends 
+BridgeActivity {
+    @Override protected void 
+    onCreate(Bundle 
+    savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override public void 
+    onBackPressed() {
+        if (getBridge() != null 
+        &&
+            getBridge().getWebView() 
+            != null &&
+            getBridge().getWebView().canGoBack()) 
+            { 
+            getBridge().getWebView().goBack();
+        } else {
+            finishAffinity(); // 
+            closes the app 
+            completely
+        }
+    }
+}
